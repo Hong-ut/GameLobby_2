7aac1a0941fb313277355f33e06875255495c539
Hong-ut <7594hsj@gmail.com>
2022-11-16 17:54:20 -0500

    update1

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,5 +1,39 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react';
 
-const AppContext = createContext(null);
+const AppContext = createContext();
 
-export default AppContext;
+const AppContextProvider = ({children}) => {
+    const [players, setPlayers] = useState([
+        {
+            id: 1,
+            color: 'white'
+        },
+        {
+            id: 2,
+            color: 'white'
+        },
+        {
+            id: 3,
+            color: 'white'
+        },
+        {
+            id: 4,
+            color: 'white'
+        }
+    ]);
+    
+    const [colors, setColors] = useState([
+        { value: 'red', label: 'red', disabled: false },
+        { value: 'blue', label: 'blue', disabled: false },
+        { value: 'green', label: 'green', disabled: false },
+        { value: 'purple', label: 'purple', disabled: false }
+      ]);
+
+    return (
+        <AppContext.Provider value = {{players, setPlayers, colors, setColors}}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export { AppContext, AppContextProvider };
