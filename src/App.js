aed2721ca8af4c3fb88c851dc0b6ff3d475a9d6a
Hong-ut <7594hsj@gmail.com>
2022-11-26 15:00:12 -0500

    auth

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@
 //import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import Lobby from './components/Lobby';
 import { AppContextProvider } from './components/Context';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from 'react';
+import Login from './components/Login';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   // All hooks are defined in App.js (highest component & Provided as context in the return statement)
 
-  
+
 
   return (
     // NOTE: this context is also provided for its grandchildren (X need several Providers)
-      <AppContextProvider>
-        <Lobby />
-          
-      </AppContextProvider>
+
+    <AppContextProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </AppContextProvider>
+
   );
 }
 
