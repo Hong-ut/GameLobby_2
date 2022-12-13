cb7041ec7a899578179179d1037e47d5bc3e1f95
Hong-ut <7594hsj@gmail.com>
2022-12-13 01:06:50 -0500

    Need Luke to take a look at CORS policy bug

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,20 +1,67 @@
 //TODO: use Material UI (from videos)
 
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Select from 'react-select'
 import { Container } from '@material-ui/core'
 import { AppContext, AppContextProvider } from './Context'
+import axios from 'axios';
 
 
 const Player = ({ player }) => { //onDelete is a function passed up (to the parent file)
     //onDelete = passed up (executed in parent func) while task = passed down (executed here)
-    const {colors, setColors, players, setPlayers} = useContext(AppContext)
+    const { colors, setColors, players, setPlayers } = useContext(AppContext)
     //const docRef = doc(db, 'users', auth.doc)
     // use Axios to connect to cloud functions, which connects to firestore
     // getcolors (getdoc from firestore), setcolors (updatedoc)
     // URL => Axios side 
 
-    // const [playerColors, setPlayerColors] = useState([])
+    const getColorURL = "https://us-central1-gamelobby-ecdf4.cloudfunctions.net/getColor?text=5qsdr5xIQRgFscpwOPvf";
+    const [postColorURL, setPostColorURL] = useState('');
+    // user's saved colors
+    useEffect(() => {
+        axios.get(getColorURL)
+            .then(response => {
+                const playerColors = response.data.result
+                console.log(response.data.result);
+                setPlayers([{
+                    id: 1,
+                    color: playerColors[0]
+                },
+                {
+                    id: 2,
+                    color: playerColors[1]
+                },
+                {
+                    id: 3,
+                    color: playerColors[2]
+                },
+                {
+                    id: 4,
+                    color: playerColors[3]
+                }])
+                console.log("players: ", players)
+                console.log("colors: ", colors)
+            })
+            .catch(error => {
+                // Handle any errors
+            });
+    }, [])
+
+    useEffect(() => {
+        console.log(postColorURL);
+        if (postColorURL != "") {
+            axios.get(postColorURL)
+                .then(response => {
+                    console.log("data posted to firebase");
+                })
+                .catch(error => {
+                    // Handle any errors
+                });
+        }
+    }, [postColorURL]
+    )
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
 
     return (
         <Container maxWidth="lg">
@@ -31,18 +78,21 @@ const Player = ({ player }) => { //onDelete is a function passed up (to the pare
                             ))
                         if (player.color != "white") {
                             setColors(
-                                colors.map((color) => 
-                                    color.value === player.color ? { ...color, disabled: false}
+                                colors.map((color) =>
+                                    color.value === player.color ? { ...color, disabled: false }
                                         : color
                                 )
                             )
-                            console.log(player.color)
                         }
+                        console.log("TEST: ", e.value, player.id);
+                        setPostColorURL(`https://us-central1-gamelobby-ecdf4.cloudfunctions.net/postColor?text=5qsdr5xIQRgFscpwOPvf,${player.id},${e.value}`);
+                        console.log(postColorURL);
                         // ****************
                         //QUESTION: WHY DOES the code below WORK? I didn't use setColors here...
                         //I didn't use setColors but colors (referred by e here) still changes...
                         // *************
                         e.disabled = true;
+
                     }}
                     isOptionDisabled={(option) => option.disabled}
                 />
